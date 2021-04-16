0c88649662aa8638346ccf03f82886c21e4a4a67
laanatri <laanatri.ade@gmail.com>
2021-04-16 16:56:39 +0200

    filter

--- react-course/src/components/Expenses/Expenses.js
+++ react-course/src/components/Expenses/Expenses.js
@@ -1,5 +1,8 @@
+import React, { useState } from 'react';
+
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 
 import './Expenses.css';
 
@@ -13,8 +16,15 @@ const Expenses = (props) => {
     </div>
   );
 
+  const [filteredYear, setFilteredYear] = useState(2020);
+
+  const filterChangeHandler = selectedYear => {
+    setFilteredYear(selectedYear);
+  }
+
   return (
     <Card className="expenses">
+      <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
       {allExpenses}
     </Card>
   )
